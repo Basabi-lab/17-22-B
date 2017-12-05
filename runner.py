0adfce9f3e4ab8396bc5a2b768f9589d8a7c0adc
kokoax <yomo4808@gmail.com>
2017-12-05 20:31:43 +0900

    Fix 倉庫の状態を表示するメソッドを呼び出していなかったので修正

--- runner.py
+++ runner.py
@@ -14,6 +14,7 @@ class Runner:
         place_id_list = self.recog.recog(imgpath)
         parts_id = self.store.store(''.join(place_id_list), amount)
         self.plot.plotAt(parts_id)
+        self.plot.plot()
 
 class Scanner:
     def __init__(self, scan_dir):
