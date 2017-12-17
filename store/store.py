c25d0f520b05311aa5ce8f9ed9dbbfe33b1b55c4
kokoax <yomo4808@gmail.com>
2017-12-18 06:33:52 +0900

    Add 12/06のやつ

--- store/store.py
+++ store/store.py
@@ -1,7 +1,13 @@
+storage = []#在庫を管理する配列
+
 class Store():
     def store(self, partId, amount):
         # 部品IDと個数を受け取って，場所IDを返す
-        pass
+
+        for i in storage:
+            for j in i:
+                if j == partId:#入力した部品idが在庫にある時
+                    pass
 
     def lookup(self, placeId):
         # 場所IDを受け取って，[部品ID,個数]を返す
